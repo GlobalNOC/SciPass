0aae35f4ef07c3a3be2d120821c9c838c3b933e3
AJ Ragusa <ajragusa@gmail.com>
2015-10-30 20:35:35 +0000

    we were missing some tests

--- python/t/Test.py
+++ python/t/Test.py
@@ -20,8 +20,7 @@ if __name__ == '__main__':
     balancer_only_tests = BalancerOnlyTest.suite()
     simple_balancer_only_tests = SimpleBalancerOnlyTest.suite()
     inline_tests = InlineTest.suite()
-    #suite = unittest.TestSuite([scipasstests, simplebalancertests, balancer_only_tests, inline_tests, simple_balancer_only_tests])
-    suite = unittest.TestSuite([simple_balancer_only_tests])
-
+    suite = unittest.TestSuite([scipasstests, simplebalancertests, balancer_only_tests, inline_tests, simple_balancer_only_tests])
+    
     xmlrunner.XMLTestRunner(output='test-reports').run(suite)
 
